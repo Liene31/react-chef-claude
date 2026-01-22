abc4869a63e5e998d105b33bd9d4b12ab3c87e0a
Liene <lienebare@gmail.com>
2026-01-22 09:21:36 +0100

    add recipe section

--- components/Main.jsx
+++ components/Main.jsx
@@ -14,7 +14,7 @@ export function Main() {
     });
   }
 
-  ///////////////////////////
+  console.log(ingredients.length);
 
   return (
     <main>
@@ -27,7 +27,24 @@ export function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>{ingredientsList}</ul>
+
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsList}
+          </ul>
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      )}
     </main>
   );
 }
